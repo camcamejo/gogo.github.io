c10ded4bb86ac447bf512d79632b642f3f30c224
Camp Sci <info@camp-sci.com>
2019-05-10 16:44:56 -0400

    Working title animation, added deal cards, added category icons, added footer list

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -16,13 +16,18 @@ export class HomeComponent implements OnInit {
 
     ngOnInit() {
       $(document).ready(function() {
-        $(".splash-title").lettering();
+        $(".splash-title").lettering('lines');
         $(".splash-button").lettering();
       });
       
       $(document).ready(function() {
           animation();
-      }, 1000);
+          setTimeout(function(){
+            $('html, body').animate({
+              scrollTop:$("#deals").offset().top}, 2000);
+              return false;
+          }, 2000);
+      }, 5000);
       
       $('.splash-button').click(function() {
           animation();
@@ -34,7 +39,7 @@ export class HomeComponent implements OnInit {
           title1.to(".splash-button", 0, {visibility: 'hidden', opacity: 0})
           title1.staggerFromTo(".splash-title span", 0.5, 
           {ease: Back.easeOut.config(1.7), opacity: 0, bottom: -80},
-          {ease: Back.easeOut.config(1.7), opacity: 1, bottom: 0}, 0.05);
+          {ease: Back.easeOut.config(1.7), opacity: 1, bottom: 0}, 0.5);
           title1.to(".splash-button", 0.2, {visibility: 'visible' ,opacity: 1})
       }
       
